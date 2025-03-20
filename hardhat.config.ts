1147e754f5098888acba3b67c24cb59d2640ae2e
LucasR29 <lrigon2904@gmail.com>
2025-03-20 06:50:43 -0300

    remv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import '@openzeppelin/hardhat-upgrades';
-import 'dotenv/config';
-import dotenv from 'dotenv';
+import "@openzeppelin/hardhat-upgrades";
+import "dotenv/config";
+import dotenv from "dotenv";
 dotenv.config();
 
 const config: HardhatUserConfig = {
@@ -11,8 +11,8 @@ const config: HardhatUserConfig = {
     amoy: {
       chainId: 80002,
       url: "https://rpc-amoy.polygon.technology",
-      accounts: ["0x65bff551a90eb9301cbd747fcff300862a38e700da5af572600d78ac5b21a19e"],
-    }
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
   },
 };
 
